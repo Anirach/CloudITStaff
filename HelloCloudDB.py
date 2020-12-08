37d19236c0032e21fc2c399306c1bcc88dfecf58
Anirach Mingkhwan <anirachmingkhwan@Anirach-MacBook-Pro.local>
2020-12-08 21:08:09 +0700

    check

--- HelloCloudDB.py
+++ HelloCloudDB.py
@@ -92,7 +92,7 @@ def delete_staff(id):
 
 @app.route('/', methods=['GET'])
 def get():
-    return jsonify({'ms': 'Hello Cloud DB'})
+    return jsonify({'ms': 'Hello Cloud DB1'})
 
 # Run Server
 if __name__ == "__main__":
